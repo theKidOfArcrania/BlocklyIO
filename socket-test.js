4afe0ecb70baf16f2c99cd900ff1b8f0a8599c7f
Henry Wang <wanghenry1998@gmail.com>
2017-02-23 22:29:16 -0600

    Make github pages as a demo

--- socket-test.js
+++
@@ -1,4 +0,0 @@
-var socket = io("http://localhost:8081");
-socket.on('connect', document.write.bind(document, "<p>Connected!"));
-socket.on('message', function(data) {document.write("<p>Message: " + data)});
-socket.on('disconnect', document.write.bind(this, "<p>Disconnected!"));
